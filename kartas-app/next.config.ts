0f555c7c3a3e27482ff9d5f38044dfd7b91d307a
andreicatrina <Catrina.andrei93@yahoo.com>
2025-12-15 13:53:26 +0200

    test

--- kartas-app/next.config.ts
+++ kartas-app/next.config.ts
@@ -1,7 +1,25 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
+
+/*
+
+@type {import('next').NextConfig}
+
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+};
+
+module.exports = nextConfig;
+
+
+
+*/
