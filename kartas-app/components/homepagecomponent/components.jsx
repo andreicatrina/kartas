f7d4ebf73ee5ec79ecf28fcc72d2071a4e9d1148
andreicatrina <Catrina.andrei93@yahoo.com>
2025-12-16 14:00:48 +0200

    test

---
+++ kartas-app/components/homepagecomponent/components.jsx
@@ -0,0 +1,17 @@
+import { styled } from "styled-components";
+
+export const HomeSection = styled.section`
+  width: 100%;
+  min-height: 100vh;
+  background-color: #0a0a0a;
+`;
+
+export const HomeContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  padding: 16px 24px;
+  background-color: #0a0a0a;
+  border-bottom: 1px solid #dfad6a;
+`;
--- kartas-app/pages/index.tsx
+++ kartas-app/pages/index.tsx
@@ -1,17 +1,9 @@
 import Image from "next/image";
 
-import {
-  HomeContainer,
-  HomeSection,
-  LandingContainer,
-  LandingParentContainer,
-  LandingSpan,
-  LandingSpanContainer,
-  LandingTitle,
-  LandingTitleContainer,
-} from "./components.jsx";
+import { HomeContainer, HomeSection } from "../components/homepagecomponent/components.jsx";
 
 import Header from "../components/header";
+import Landing from "../components/landing";
 
 export default function Home() {
   return (
@@ -19,23 +11,7 @@ export default function Home() {
       <HomeContainer>
         <Header />
       </HomeContainer>
-      <LandingParentContainer>
-        <LandingContainer>
-          <LandingTitleContainer>
-            <LandingTitle>Kartas Bucharest Events</LandingTitle>
-          </LandingTitleContainer>
-          <LandingSpanContainer>
-            <LandingSpan>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem rem saepe quaerat debitis fuga. Accusamus
-              reiciendis minus obcaecati illo commodi, atque illum, dolores et delectus aperiam non inventore ipsam
-              beatae iusto recusandae magnam est amet repellendus numquam odio quod temporibus ullam exercitationem
-              blanditiis. Autem obcaecati facilis nulla non necessitatibus, culpa commodi nam incidunt consequuntur
-              neque illum quasi itaque voluptate! Molestias labore, iure fugit minus, sint tenetur vitae laudantium
-              dolore autem totam tempora accusamus excepturi nostrum culpa veritatis eum atque consequuntur.
-            </LandingSpan>
-          </LandingSpanContainer>
-        </LandingContainer>
-      </LandingParentContainer>
+      <Landing />
     </HomeSection>
   );
 }
