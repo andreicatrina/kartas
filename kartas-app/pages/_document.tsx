16a1b9dd1022d3ccf0780fd1eed4c1dfd980b589
andreicatrina <Catrina.andrei93@yahoo.com>
2026-01-07 19:28:43 +0200

    analytics added

--- kartas-app/pages/_document.tsx
+++ kartas-app/pages/_document.tsx
@@ -1,5 +1,8 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
+import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+
 export default function Document() {
   return (
     <Html>
@@ -21,6 +24,8 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
+        <Analytics />
+        <SpeedInsights />
       </body>
     </Html>
   );
